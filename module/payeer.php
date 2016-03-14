8015fdcc4a99eae27e5a0310683dcfea62381e22
dldemoks <demokstar@gmail.com>
2016-03-14 19:49:35 +0700

    update

--- module/payeer.php
+++ module/payeer.php
@@ -1,12 +1,37 @@
 <?php
-
 require ('includes/application_top.php');
 
 if (isset($_POST["m_operation_id"]) && isset($_POST["m_sign"]))
 {
-	$m_key = MODULE_PAYMENT_PAYEER_SECRETKEY;
+	$err = false;
+	$message = '';
+
+	// запись логов
+
+	$log_text = 
+	"--------------------------------------------------------\n" .
+	"operation id		" . $_POST['m_operation_id'] . "\n" .
+	"operation ps		" . $_POST['m_operation_ps'] . "\n" .
+	"operation date		" . $_POST['m_operation_date'] . "\n" .
+	"operation pay date	" . $_POST['m_operation_pay_date'] . "\n" .
+	"shop				" . $_POST['m_shop'] . "\n" .
+	"order id			" . $_POST['m_orderid'] . "\n" .
+	"amount				" . $_POST['m_amount'] . "\n" .
+	"currency			" . $_POST['m_curr'] . "\n" .
+	"description		" . base64_decode($_POST['m_desc']) . "\n" .
+	"status				" . $_POST['m_status'] . "\n" .
+	"sign				" . $_POST['m_sign'] . "\n\n";
 	
-	$arHash = array(
+	$log_file = MODULE_PAYMENT_PAYEER_LOG;
+	
+	if (!empty($log_file))
+	{
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . $log_file, $log_text, FILE_APPEND);
+	}
+	
+	// проверка цифровой подписи и ip
+
+	$sign_hash = strtoupper(hash('sha256', implode(":", array(
 		$_POST['m_operation_id'],
 		$_POST['m_operation_ps'],
 		$_POST['m_operation_date'],
@@ -17,107 +42,116 @@ if (isset($_POST["m_operation_id"]) && isset($_POST["m_sign"]))
 		$_POST['m_curr'],
 		$_POST['m_desc'],
 		$_POST['m_status'],
-		$m_key
-	);
-	$sign_hash = strtoupper(hash('sha256', implode(":", $arHash)));
+		MODULE_PAYMENT_PAYEER_SECRETKEY
+	))));
 	
-	// проверка принадлежности ip списку доверенных ip
+	$valid_ip = true;
+	$sIP = str_replace(' ', '', MODULE_PAYMENT_PAYEER_IPFILTER);
 	
-	$list_ip_str = str_replace(' ', '', MODULE_PAYMENT_PAYEER_IPFILTER);
-	
-	if (!empty($list_ip_str)) 
+	if (!empty($sIP))
 	{
-		$list_ip = explode(',', $list_ip_str);
-		$this_ip = $_SERVER['REMOTE_ADDR'];
-		$this_ip_field = explode('.', $this_ip);
-		$list_ip_field = array();
-		$i = 0;
-		$valid_ip = FALSE;
-		foreach ($list_ip as $ip)
+		$arrIP = explode('.', $_SERVER['REMOTE_ADDR']);
+		if (!preg_match('/(^|,)(' . $arrIP[0] . '|\*{1})(\.)' .
+		'(' . $arrIP[1] . '|\*{1})(\.)' .
+		'(' . $arrIP[2] . '|\*{1})(\.)' .
+		'(' . $arrIP[3] . '|\*{1})($|,)/', $sIP))
 		{
-			$ip_field[$i] = explode('.', $ip);
-			if ((($this_ip_field[0] ==  $ip_field[$i][0]) or ($ip_field[$i][0] == '*')) and
-				(($this_ip_field[1] ==  $ip_field[$i][1]) or ($ip_field[$i][1] == '*')) and
-				(($this_ip_field[2] ==  $ip_field[$i][2]) or ($ip_field[$i][2] == '*')) and
-				(($this_ip_field[3] ==  $ip_field[$i][3]) or ($ip_field[$i][3] == '*')))
-				{
-					$valid_ip = TRUE;
+			$valid_ip = false;
+		}
+	}
+	
+	if (!$valid_ip)
+	{
+		$message .= " - ip-адрес сервера не является доверенным\n" .
+		"   доверенные ip: " . $sIP . "\n" .
+		"   ip текущего сервера: " . $_SERVER['REMOTE_ADDR'] . "\n";
+		$err = true;
+	}
+
+	if ($_POST['m_sign'] != $sign_hash)
+	{
+		$message .= " - не совпадают цифровые подписи\n";
+		$err = true;
+	}
+	
+	if (!$err)
+	{
+		// загрузка заказа
+		
+		$order_id = preg_replace('/[^a-zA-Z0-9_-]/', '', substr($_POST['m_orderid'], 0, 32));
+		
+		$curr_query = tep_db_query('select currency from ' . TABLE_ORDERS . ' where orders_id = "' . $order_id . '"');
+		$amount_query = tep_db_query('select final_price from ' . TABLE_ORDERS_PRODUCTS . ' where orders_id = "' . $order_id . '"');
+		$curr_query_row = tep_db_fetch_array($curr_query);
+		$amount_query_row = tep_db_fetch_array($amount_query);
+		
+		$order_curr = ($curr_query_row['currency'] == 'RUR') ? 'RUB' : $curr_query_row['currency'];
+		$order_amount = number_format($amount_query_row['final_price'], 2, '.', '');
+
+		// проверка суммы и валюты
+	
+		if ($_POST['m_amount'] != $order_amount)
+		{
+			$message .= " - неправильная сумма\n";
+			$err = true;
+		}
+
+		if ($_POST['m_curr'] != $order_curr)
+		{
+			$message .= " - неправильная валюта\n";
+			$err = true;
+		}
+
+		// проверка статуса
+		
+		if (!$err)
+		{
+			switch ($_POST['m_status'])
+			{
+				case 'success':
+					$status = MODULE_PAYMENT_PAYEER_ORDER_STATUS;
+					$comment = 'Заказ успешно оплачен через Payeer';
 					break;
-				}
-			$i++;
+					
+				default:
+					$status = MODULE_PAYMENT_PAYEER_ORDER_STATUS_FAIL;
+					$comment = 'Заказ оплачен неуспешно через Payeer';
+					$message .= ' - статус платежа не является success' . "\n";
+					$err = true;
+					break;
+			}
+			
+			$sql_data_array = array('orders_status' => $status);
+			tep_db_perform('orders', $sql_data_array, 'update', "orders_id='" . $_POST['m_orderid'] . "'");
+			
+			$sql_data_arrax = array(
+				'orders_id' => $_POST['m_orderid'], 
+				'orders_status_id' => $status, 
+				'date_added' => 'now()', 
+				'customer_notified' => '0', 
+				'comments' => $comment
+			);
+			tep_db_perform('orders_status_history', $sql_data_arrax);
 		}
-	}
-	else
-	{
-		$valid_ip = TRUE;
-	}
-		
-	$log_text = 
-		"--------------------------------------------------------\n".
-		"operation id		" . $_POST["m_operation_id"] . "\n".
-		"operation ps		" . $_POST["m_operation_ps"] . "\n".
-		"operation date		" . $_POST["m_operation_date"] . "\n".
-		"operation pay date	" . $_POST["m_operation_pay_date"] . "\n".
-		"shop				" . $_POST["m_shop"] . "\n".
-		"order id			" . $_POST["m_orderid"] . "\n".
-		"amount				" . $_POST["m_amount"] . "\n".
-		"currency			" . $_POST["m_curr"] . "\n".
-		"description		" . base64_decode($_POST["m_desc"]) . "\n".
-		"status				" . $_POST["m_status"] . "\n".
-		"sign				" . $_POST["m_sign"] . "\n\n";
-
-	if (MODULE_PAYMENT_PAYEER_LOG != '')
-	{
-		file_put_contents($_SERVER['DOCUMENT_ROOT'] . MODULE_PAYMENT_PAYEER_LOG, $log_text, FILE_APPEND);
-	}
-
-	if ($_POST["m_sign"] == $sign_hash && $_POST['m_status'] == "success" && $valid_ip)
-	{
-		$status = MODULE_PAYMENT_PAYEER_ORDER_STATUS;
-		$comment = 'Заказ успешно оплачен через Payeer';
-		echo ($_POST['m_orderid'] . '|success');
-	}
-	else
-	{
-		$status = MODULE_PAYMENT_PAYEER_ORDER_STATUS_FAIL;
-		$comment = 'Заказ оплачен неуспешно через Payeer';
-		
-		$to = MODULE_PAYMENT_PAYEER_EMAILERROR;
-		$subject = 'Ошибка оплаты';
-		$message = 'Не удалось провести платёж через систему Payeer по следующим причинам:' . "\n\n";
-		
-		if ($_POST["m_sign"] != $sign_hash)
-		{
-			$message .= ' - Не совпадают цифровые подписи' . "\n";
-		}
-		
-		if ($_POST['m_status'] != "success")
-		{
-			$message .= ' - Cтатус платежа не является success' . "\n";
-		}
-		
-		if (!$valid_ip)
-		{
-			$message .= ' - ip-адрес сервера не является доверенным' . "\n";
-			$message .= '   доверенные ip: ' . MODULE_PAYMENT_PAYEER_IPFILTER . "\n";
-			$message .= '   ip текущего сервера: ' . $_SERVER['REMOTE_ADDR'] . "\n";
-		}
-		
-		$message .= "\n" . $log_text;
-		$headers = "From: no-reply@" . $_SERVER['HTTP_SERVER'] . "\r\nContent-type: text/plain; charset=utf-8 \r\n";
-		mail($to, $subject, $message, $headers);
-		echo ($_POST['m_orderid'] . '|error');
 	}
 	
-	$sql_data_array = array('orders_status' => $status);
-	tep_db_perform('orders', $sql_data_array, 'update', "orders_id='" . $_POST['m_orderid'] . "'");
-	$sql_data_arrax = array(
-		'orders_id' => $_POST['m_orderid'], 
-		'orders_status_id' => $status, 
-		'date_added' => 'now()', 
-		'customer_notified' => '0', 
-		'comments' => $comment
-	);
-	tep_db_perform('orders_status_history', $sql_data_arrax);
+	if ($err)
+	{
+		$to = MODULE_PAYMENT_PAYEER_EMAILERROR;
+
+		if (!empty($to))
+		{
+			$message = "Не удалось провести платёж через систему Payeer по следующим причинам:\n\n" . $message . "\n" . $log_text;
+			$headers = "From: no-reply@" . $_SERVER['HTTP_HOST'] . "\r\n" . 
+			"Content-type: text/plain; charset=utf-8 \r\n";
+			mail($to, 'Ошибка оплаты', $message, $headers);
+		}
+
+		exit($_POST['m_orderid'] . '|error');
+	}
+	else
+	{
+		exit($_POST['m_orderid'] . '|success');
+	}
 }
 ?>
